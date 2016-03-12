08618c7a5d8e65f1ae3e89047b3da734de7857d7
Andrew Menning <menning23@gmail.com>
2016-03-12 18:12:38 -0500

    fix steal worm action to require worm in frozen dice

--- app/assets/js/services/check-valid-worm-steal.js
+++ app/assets/js/services/check-valid-worm-steal.js
@@ -7,8 +7,8 @@ angular.module('pickominoGame')
 		return {
 			validate: function(wormValue){
 				var enoughDiceValue = (wormValue === FrozenDiceArray.frozenStatus.sum);
-				var foundFrozenWormDie = $filter('filter')(FrozenDiceArray.array, {value: 6}, true);					
-				return (enoughDiceValue && foundFrozenWormDie);				
+				var foundFrozenWormDie = $filter('filter')(FrozenDiceArray.array, {value: 6}, true);			
+				return (enoughDiceValue && foundFrozenWormDie.length > 0);				
 			}
 		};
 	}
