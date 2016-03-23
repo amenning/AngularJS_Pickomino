91e062458537c0ae06f359171f6700904e71effb
Andrew Menning <menning23@gmail.com>
2016-03-22 21:46:58 -0400

    separate bunk penalty into service and add to optionsCtrl

--- app/assets/js/controllers/game-action-controller.js
+++ app/assets/js/controllers/game-action-controller.js
@@ -127,23 +127,23 @@ angular.module('pickominoGame')
 			}
 		};			
 		
-		this.bunkPenalty = function(){
-			if(PlayerWormsArray.array[GameAction.status.activePlayer].length!==0){
-				//return worm to grill
-				wormValue = PlayerWormsArray.removeBunkWorm();
-				//remove highest value worm from grill
-				GrillWormsArray.addWorm(wormValue);
-				GrillWormsArray.removeBunkWorm(wormValue);
-			}
-			//reset dice and start over
-			RandomDice.resetDice();
-			GameAction.setStatus('roll', true);
-			GameAction.setStatus('takeWorm', false);
-			GameAction.setStatus('freezeDice', false);
-			GameAction.setStatus('bunk', false);
-			GameAction.switchPlayer();
-			PlayerNotification.setMessage('You can now reroll the dice.');
-			GameState.save();
-		};
+//		this.bunkPenalty = function(){
+//			if(PlayerWormsArray.array[GameAction.status.activePlayer].length!==0){
+//				//return worm to grill
+//				wormValue = PlayerWormsArray.removeBunkWorm();
+//				//remove highest value worm from grill
+//				GrillWormsArray.addWorm(wormValue);
+//				GrillWormsArray.removeBunkWorm(wormValue);
+//			}
+//			//reset dice and start over
+//			RandomDice.resetDice();
+//			GameAction.setStatus('roll', true);
+//			GameAction.setStatus('takeWorm', false);
+//			GameAction.setStatus('freezeDice', false);
+//			GameAction.setStatus('bunk', false);
+//			GameAction.switchPlayer();
+//			PlayerNotification.setMessage('You can now reroll the dice.');
+//			GameState.save();
+//		};
 	}
 ]);	
--- app/assets/js/controllers/game-player-options-controller.js
+++ app/assets/js/controllers/game-player-options-controller.js
@@ -3,7 +3,8 @@ angular.module('pickominoGame')
 .controller("PlayerOptionsController", [
 	'PlayerNumber',
 	'RollDice',
-	function(PlayerNumber, RollDice){
+	'BunkPenalty',
+	function(PlayerNumber, RollDice, BunkPenalty){
 		
 		this.setPlayers = function(playerCount){
 			PlayerNumber.set(playerCount);
@@ -12,6 +13,10 @@ angular.module('pickominoGame')
 		this.rollDice = function (){
 			RollDice.roll();
 		};
+		
+		this.bunkPenalty = function(){
+			BunkPenalty.penalize();
+		}
 		
 	}
 ]);	
